191a3e896a8d6e23a78e996b442c83f0e3aee6fe
Olga <olga.valor.wu@gmail.com>
2025-02-19 21:13:31 +0100

    dockerfile + docker compose + perfiles

---
+++ geekhub/src/test/java/com/salesianos/geekhub/GeekhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.geekhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
